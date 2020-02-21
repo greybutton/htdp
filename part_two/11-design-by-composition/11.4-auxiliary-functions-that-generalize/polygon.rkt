81b2cd268488d04779a437146b1bbb3f1c5c8186
greybutton <greybutton@yandex.com>
2020-02-21 08:34:28 +0400

    add 11.4 theory

---
+++ part_two/11-design-by-composition/11.4-auxiliary-functions-that-generalize/polygon.rkt
@@ -0,0 +1,88 @@
+(require 2htdp/image)
+
+(define triangle-p
+  (list
+    (make-posn 20 10)
+    (make-posn 20 20)
+    (make-posn 30 20)))
+
+(define square-p
+  (list
+    (make-posn 10 10)
+    (make-posn 20 10)
+    (make-posn 20 20)
+    (make-posn 10 20)))
+
+; A Polygon is one of:
+; – (list Posn Posn Posn)
+; – (cons Posn Polygon)
+
+; a plain background image 
+(define MT (empty-scene 50 50))
+ 
+; Image Polygon -> Image 
+; adds an image of p to MT
+(define (render-polygon img p)
+  (render-line (connect-dots img p) (first p) (last p)))
+
+(check-expect
+  (render-polygon MT triangle-p)
+  (scene+line
+    (scene+line
+      (scene+line MT 20 10 20 20 "red")
+      20 20 30 20 "red")
+    30 20 20 10 "red"))
+
+(check-expect
+  (render-polygon MT square-p)
+  (scene+line
+    (scene+line
+      (scene+line
+        (scene+line MT 10 10 20 10 "red")
+        20 10 20 20 "red")
+      20 20 10 20 "red")
+    10 20 10 10 "red"))
+
+; Image Posn Posn -> Image 
+; draws a red line from Posn p to Posn q into im
+(define (render-line im p q)
+  (scene+line
+    im (posn-x p) (posn-y p) (posn-x q) (posn-y q) "red"))
+
+; Polygon -> Posn
+; extracts the last item from p
+(define (last p)
+  (cond
+    [(empty? (rest (rest (rest p)))) (third p)]
+    [else (last (rest p))]))
+
+; An NELoP is one of: 
+; – (cons Posn '())
+; – (cons Posn NELoP)
+
+; Image NELoP -> Image
+; connects the Posns in p in an image
+(define (connect-dots img p)
+  (cond
+    [(empty? (rest p)) MT]
+    [else (render-line (connect-dots img (rest p))
+                       (first p)
+                       (second p))]))
+
+(check-expect
+ (connect-dots MT triangle-p)
+ (scene+line
+  (scene+line
+   (scene+line MT 20 10 20 20 "red")
+   20 20 30 20 "red")
+  30 20 20 10 "red"))
+
+(check-expect
+  (connect-dots MT square-p)
+  (scene+line
+    (scene+line
+      (scene+line
+        (scene+line MT 10 10 10 20 "red")
+        10 20 20 20 "red")
+      20 20 20 10 "red")
+    20 0 10 0 "red"))
